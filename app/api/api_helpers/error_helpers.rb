8bfb93eaab3440c5353f641efe7ec8279f5ad419
KrolikMarcin <krolikmarcin.mk@gmail.com>
2019-10-25 13:53:10 +0200

    Prepare API

---
+++ app/api/api_helpers/error_helpers.rb
@@ -0,0 +1,31 @@
+module APIHelpers
+  module ErrorHelpers
+    def error_400!(hint: nil)
+      handle_error(I18n.t('api.code_400'), 400, hint)
+    end
+
+    def error_404!(hint: nil)
+      handle_error(I18n.t('api.code_404'), 404, hint)
+    end
+
+    def error_422!(hint: nil)
+      handle_error(I18n.t('api.code_422'), 422, hint)
+    end
+
+    private
+
+    def handle_error(errors, code, hint = nil)
+      error!(
+        payload(errors, hint),
+        code
+      )
+    end
+
+    def payload(errors, hint = nil)
+      {
+        errors: Array(errors),
+        hint: hint
+      }.compact
+    end
+  end
+end
